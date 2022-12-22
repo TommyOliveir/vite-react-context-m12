80c87683af97071acf3e295374a63d5a33da0a67
TommyOliveir <laridatommy@gmail.com>
2022-12-22 16:20:39 +0100

    add

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {ThemeContextProvider} from "./themeContext"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeContextProvider>
+
+      <App />
+    </ThemeContextProvider>
   </React.StrictMode>,
 )
